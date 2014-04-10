8961fcf4b2cbd83f7b2ce72f1ff33273370a93dc
Benjamin Horsleben <benjamin@fizkerinc.dk>
2014-04-10 17:52:35 +0200

    Bumped express to 4.0.0.
    Added the missing middleware as dependencies.

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var routes = require('./routes');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var errorhandler = require('errorhandler');
 var http = require('http');
 var path = require('path');
 
@@ -15,20 +18,20 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pejs');
 app.disable('x-powered-by');
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(app.router);
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+app.get('/', routes.index);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 // TODO: add error handler
 
 // development only
 if ('development' === app.get('env')) {
-	app.use(express.errorHandler());
+	app.use(errorhandler());
 }
-
-app.get('/', routes.index);
 
 http.createServer(app).listen(app.get('port'), function () {
 	console.log('Express server listening on port ' + app.get('port'));
